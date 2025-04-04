ea6e084bc61863137952d381f3fbdb86681cd920
unimen04 <unai.mendoza@estudiante.uam.es>
2025-04-04 17:05:08 +0200

    Pequeños cambios en el structure.c

--- structure.c
+++ structure.c
@@ -1,5 +1,9 @@
 #include "structure.h"
 
+/*
+El mapa está formado por celdas de 4x4 pixeles, donde 1 representa una pared y 0 un espacio vacío.
+El mapa tiene un tamaño de 32x8 celdas, lo que equivale a 128x32 pixeles. (definido en structure.h)
+*/
 uint8_t map[MAP_HEIGHT][MAP_WIDTH]={
 	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 	{1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -11,11 +15,13 @@ uint8_t map[MAP_HEIGHT][MAP_WIDTH]={
 	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
+//x e y son las coordenadas del jugador en el mapa, no en la pantalla.
 coords set_coords(float x, float y){
 	coords coords = {x,y};
 	return coords;
 }
 
+//sabiendo la posicion del jugador en el mapa, devuelve la celda correspondiente.
 cell obtainCell(coords p1){
 	cell cellPos;
 	cellPos.x = (uint8_t)p1.x;
@@ -23,6 +29,7 @@ cell obtainCell(coords p1){
 	return cellPos;
 }
 
+//x e y dan las coordenadas del jugador en el mapa, no en la pantalla. 
 player set_player(float x, float y){
 	player j;
 	j.pos = set_coords(x,y);
@@ -31,7 +38,9 @@ player set_player(float x, float y){
 	return j;
 }
 
-//inutil en 3D
+//INUTIL EN 3D
+//convierte las coordenadas del jugador en el mapa a coordenadas de la pantalla LCD
+//la función round devuelve un tipo double, por lo que se redondea a un entero uint8_t
 posLCD coords2LCD(coords pos){
 	posLCD LCDpos;
 	double posX = round(pos.x*CELL_SIZE);
